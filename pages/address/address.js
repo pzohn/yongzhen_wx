5c7b5500776ba2eca76d0d5585bf06eb7fac2157
pzohn <pzohn@163.com>
2019-01-09 17:39:03 +0800

    20190109-01

--- pages/address/address.js
+++ pages/address/address.js
@@ -26,13 +26,14 @@ Page({
         if (res.data != 0) {
           var count = res.data.count;
           var default_id = res.data.address_defult_id;
-          if (count) {
+          if (count > 1) {
             for (var index in res.data.addresses) {
               var object = new Object();
               object.name = res.data.addresses[index].name;
               object.phone = res.data.addresses[index].phone;
               object.detail = res.data.addresses[index].detail;
               object.id = res.data.addresses[index].id;
+              object.index = index;
               if (object.id == default_id){
                 object.default = true;
               }else{
@@ -40,7 +41,20 @@ Page({
               }
               array[index] = object;
             }
-          } 
+          } else if (count == 1){
+            var object = new Object();
+            object.name = res.data.addresses.name;
+            object.phone = res.data.addresses.phone;
+            object.detail = res.data.addresses.detail;
+            object.id = res.data.addresses.id;
+            object.index = 0;
+            if (object.id == default_id) {
+              object.default = true;
+            } else {
+              object.default = false;;
+            }
+            array[0] = object;
+          }
         }
         page.setData({ address: array });
       },
@@ -57,23 +71,71 @@ Page({
       }
     })
   },
+  
+  setDefault: function (e) {
+    var index = e.currentTarget.id;
+    var object = this.data.address[index];
+    var default_flag = !object.default;
+    var page = this;
+    var app = getApp();
+    wx.request({
+      url: 'https://www.yztcc.com/updateDefaultAddress',
+      data: {
+        login_id: app.globalData.login_id,
+        id: object.id
+      },
+      method: 'POST',
+      success: function (res) {
+    
+      },
+      fail: function (res) {
+        wx.showModal({
+          title: '错误提示',
+          content: '服务器无响应，请联系工作人员!',
+          success: function (res) {
+            if (res.confirm) {
+            } else if (res.cancel) {
+            }
+          }
+        })
+      }
+    })
+  },
 
-  userInfo: function () {
+  edit: function (e) {
+    var id = e.currentTarget.id;
     wx.navigateTo({
-      url: '/pages/userInfo/userInfo'
+      url: '/pages/editaddress/editaddress?id=' + id
     })
   },
 
-  bindDateChange: function (e) {
-    this.setData({
-      date: e.detail.value
-    })
-  },
 
-  discount: function (e) {
-    var current = e.currentTarget.dataset.current;
-    this.setData({
-      tapCurrent: current
+  delete: function (e) {
+    var page = this;
+    var app = getApp();
+    wx.request({
+      url: 'https://www.yztcc.com/delAddress',
+      data: {
+        login_id: app.globalData.login_id,
+        id: e.currentTarget.id
+      },
+      method: 'POST',
+      success: function (res) {
+        wx.navigateTo({
+          url: '../address/address'
+        })
+      },
+      fail: function (res) {
+        wx.showModal({
+          title: '错误提示',
+          content: '服务器无响应，请联系工作人员!',
+          success: function (res) {
+            if (res.confirm) {
+            } else if (res.cancel) {
+            }
+          }
+        })
+      }
     })
   },
 
--- pages/editaddress/editaddress.js
+++ pages/editaddress/editaddress.js
@@ -5,23 +5,24 @@ const app = getApp()
 Page({
   data: {
     region: [],
-    default_flag:false,
-    name:'',
-    phone:'',
-    detail:''
+    name: '',
+    phone: '',
+    detail: '',
+    id:0
   },
 
-  onLoad: function () {
-      this.initRegion();
+  onLoad: function (options) {
+    var id = options.id;
+    this.initData(id);
   },
 
   backAddress: function () {
     var page = this;
     var app = getApp();
     wx.request({
-      url: 'https://www.yztcc.com/insertAddress',
+      url: 'https://www.yztcc.com/updateAddress',
       data: {
-        login_id: app.globalData.login_id,
+        id: page.data.id,
         name: page.data.name,
         phone: page.data.phone,
         province: page.data.region[0],
@@ -56,8 +57,8 @@ Page({
   },
 
   accountInput: function (e) {
-    if (e.currentTarget.id == 1){
-      this.setData({ name: e.detail.value});
+    if (e.currentTarget.id == 1) {
+      this.setData({ name: e.detail.value });
     } else if (e.currentTarget.id == 2) {
       this.setData({ phone: e.detail.value });
     } else if (e.currentTarget.id == 3) {
@@ -65,18 +66,39 @@ Page({
     }
   },
 
-  initRegion: function () {
-    var arry = [];
+  initData: function (id) {
+    var page = this;
     var app = getApp();
-    arry[0] = app.globalData.province;
-    arry[1] = app.globalData.city;
-    arry[2] = app.globalData.district;
-    this.setData({region:arry});
-  },
-
-  switch2Change: function (e) {
-    this.setData({
-      default_flag: e.detail.value
+    wx.request({
+      url: 'https://www.yztcc.com/getAddress',
+      data: {
+        id: id
+      },
+      method: 'POST',
+      success: function (res) {
+        var region = [];
+        region[0] = res.data.province;
+        region[1] = res.data.city;
+        region[2] = res.data.area;
+        page.setData({ 
+          name: res.data.name,
+          phone: res.data.phone,
+          detail: res.data.detail,
+          region: region,
+          id:id
+         });
+      },
+      fail: function (res) {
+        wx.showModal({
+          title: '错误提示',
+          content: '服务器无响应，请联系工作人员!',
+          success: function (res) {
+            if (res.confirm) {
+            } else if (res.cancel) {
+            }
+          }
+        })
+      }
     })
   },
 })
