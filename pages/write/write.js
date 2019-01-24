0bd254ee5dc4c457576755d9cd98500417fcc9b5
pzohn <pzohn@163.com>
2019-01-24 23:25:58 +0800

    20190124-2

--- pages/write/write.js
+++ pages/write/write.js
@@ -2,53 +2,30 @@ Page({
   data: {
     img_url: [],
     content: '',
-    source: ''
-  },
-
-  uploadimg: function () {
-    var that = this;
-    wx.chooseImage({  //从本地相册选择图片或使用相机拍照
-      count: 1, // 默认9
-      sizeType: ['original', 'compressed'], // 可以指定是原图还是压缩图，默认二者都有
-      sourceType: ['album', 'camera'], // 可以指定来源是相册还是相机，默认二者都有
-
-      success: function (res) {
-        //console.log(res)
-        //前台显示
-        that.setData({
-          source: res.tempFilePaths
-        })
-
-        // 返回选定照片的本地文件路径列表，tempFilePath可以作为img标签的src属性显示图片
-        var tempFilePaths = res.tempFilePaths
-        wx.uploadFile({
-          url: 'https://www.yztcc.com/upload',
-          filePath: tempFilePaths[0],
-          name: 'file',
-
-          success: function (res) {
-            //打印
-            console.log(res.data)
-          }
-        })
-
-      }
-    })
+    title:''
   },
 
   onLoad: function (options) {
 
   },
+
   input: function (e) {
     this.setData({
       content: e.detail.value
     })
   },
+
+  title: function (e) {
+    this.setData({
+      title: e.detail.value
+    })
+  },
+  
   chooseimage: function () {
     var that = this;
     wx.chooseImage({
       count: 9, // 默认9  
-      sizeType: ['original', 'compressed'], // 可以指定是原图还是压缩图，默认二者都有  
+      sizeType: ['compressed'], // 可以指定是原图还是压缩图，默认二者都有  
       sourceType: ['album', 'camera'], // 可以指定来源是相册还是相机，默认二者都有  
       success: function (res) {
 
@@ -75,21 +52,57 @@ Page({
           })
 
         }
-
       }
     })
   },
   //发布按钮事件
   send: function () {
+    var app = getApp();
+    if (app.globalData.loginFlag == false) {
+      wx.navigateTo({
+        url: '../login/login'
+      });
+      return;
+    }
     var that = this;
-    var user_id = wx.getStorageSync('userid')
+    var wxUserInfo = wx.getStorageSync('wxUserInfo');
     wx.showLoading({
       title: '上传中',
     })
-    that.img_upload()
+    console.log(wxUserInfo.nickName);
+    console.log(wxUserInfo.avatarUrl);
+    console.log(that.data.content);
+    console.log(that.data.title);
+    console.log(app.globalData.login_id);
+    wx.request({
+      url: 'https://www.yztcc.com/insertTopic',
+      data: {
+        loginname: wxUserInfo.nickName,
+        avatar_url: wxUserInfo.avatarUrl,
+        content:that.data.content,
+        title: that.data.title,
+        login_id: app.globalData.login_id,
+        tab:1
+      },
+      method: 'POST',
+      success: function (res) {
+        that.img_upload(app.globalData.login_id)
+      },
+      fail: function (res) {
+        wx.showModal({
+          title: '错误提示',
+          content: '服务器无响应，请联系工作人员!',
+          success: function (res) {
+            if (res.confirm) {
+            } else if (res.cancel) {
+            }
+          }
+        })
+      }
+    })
   },
   //图片上传
-  img_upload: function () {
+  img_upload: function (id) {
     let that = this;
     let img_url = that.data.img_url;
     let img_url_ok = [];
@@ -97,44 +110,20 @@ Page({
     for (let i = 0; i < img_url.length; i++) {
       wx.uploadFile({
         //路径填你上传图片方法的地址
-        url: 'http://wechat.homedoctor.com/Moments/upload_do',
+        url: 'https://www.yztcc.com/upload',
         filePath: img_url[i],
         name: 'file',
         formData: {
-          'user': 'test'
+          'parent_id': id
         },
         success: function (res) {
+          console.log(res.data);
           console.log('上传成功');
           //把上传成功的图片的地址放入数组中
           img_url_ok.push(res.data)
           //如果全部传完，则可以将图片路径保存到数据库
           if (img_url_ok.length == img_url.length) {
-            var userid = wx.getStorageSync('userid');
-            var content = that.data.content;
-            wx.request({
-              url: 'http://wechat.homedoctor.com/Moments/adds',
-              data: {
-                user_id: userid,
-                images: img_url_ok,
-                content: content,
-              },
-              success: function (res) {
-                if (res.data.status == 1) {
-                  wx.hideLoading()
-                  wx.showModal({
-                    title: '提交成功',
-                    showCancel: false,
-                    success: function (res) {
-                      if (res.confirm) {
-                        wx.navigateTo({
-                          url: '/pages/my_moments/my_moments',
-                        })
-                      }
-                    }
-                  })
-                }
-              }
-            })
+            wx.hideLoading();
           }
         },
         fail: function (res) {
